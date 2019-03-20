ec2319c3222d1ddfdaf79129f05d2f618807fded
MrYdse <er.yd@outlook.com>
2019-03-20 13:00:07 +0100

    Move connectionstring for better readability

--- fitboi/src/main/java/app/Sql.java
+++ fitboi/src/main/java/app/Sql.java
@@ -5,6 +5,11 @@ import java.util.Properties;
 
 public abstract class Sql {
 
+    /**
+     *
+     */
+
+    private static final String connectionString = "jdbc:mysql://localhost:3306/database?autoReconnect=true&allowPublicKeyRetrieval=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     protected Connection connection;
 
     public Sql() {
@@ -16,7 +21,7 @@ public abstract class Sql {
             Properties props = new Properties();
             props.put("user", "fitboi");
             props.put("password", "123");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database?autoReconnect=true&allowPublicKeyRetrieval=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", props);
+            connection = DriverManager.getConnection(connectionString, props);
         } catch (Exception e) {
             throw new RuntimeException("Unable to connect", e);
         }
