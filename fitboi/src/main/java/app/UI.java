fe98a4d99fc2c79ef6df9e5eb7790879ac1d16f9
axelk <axelkjnsberg@gmail.com>
2019-03-20 14:10:55 +0100

    Make UI logic

--- fitboi/src/main/java/app/UI.java
+++ fitboi/src/main/java/app/UI.java
@@ -2,14 +2,19 @@ package app;
 
 import java.sql.*;
 import java.io.PrintStream;
+import java.util.*;
 
 public class UI extends Sql {
 
     private PrintStream printStream;
 
+    private Map<String, String> methods = new HashMap<String, String>();
+    private String format = "%-40s%s%n";
+
     public UI(PrintStream stream) {
         this.printStream = stream;
         this.connect();
+        methods.put("Help", "Prints list of methods");
     }
 
     public void handleInput(String input) {
@@ -17,6 +22,10 @@ public class UI extends Sql {
             case "hi":      printStream.print("Hi yourself");
                             break;
             case "test":    printStream.print("Yup, that's a test");
+                            break;
+            case "help":    for (Map.Entry<String, String> entry : methods.entrySet()) {
+                              System.out.printf(format, entry.getKey(), entry.getValue());
+                            }
                             break;
         }
     }
