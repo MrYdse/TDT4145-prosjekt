3ea7f285053f8d1e853b43379e3ffe1d779fc290
axelk <axelkjnsberg@gmail.com>
2019-03-20 16:13:02 +0100

    Update UI
    Numbers lol

--- fitboi/src/main/java/app/UI.java
+++ fitboi/src/main/java/app/UI.java
@@ -25,7 +25,7 @@ public class UI extends Sql {
 
     public void handleInput(String input) {
         ArrayList<String> args = new ArrayList<String>(Arrays.asList(input.toLowerCase().split(" ")));
-          if (args.size() == 0){
+          if (args.size() == 1){
             switch(args.get(0)) {
             case "help":    for (Map.Entry<String, String> entry : methods.entrySet()) {
                                   System.out.printf(format, entry.getKey(), entry.getValue());
@@ -40,7 +40,7 @@ public class UI extends Sql {
             }
           }
 
-          if (args.size() == 1){
+          if (args.size() == 2){
             switch(args.get(0)) {
             case "addexercisegroup": addExerciseGroup(args.get(1));
                             break;
@@ -51,7 +51,7 @@ public class UI extends Sql {
             }
           }
 
-          if (args.size() == 2){
+          if (args.size() == 3){
             switch(args.get(0)) {
             case "addmachine": addMachine(args.get(1), args.get(2));
                             break;
@@ -62,7 +62,7 @@ public class UI extends Sql {
             }
           }
 
-          if (args.size() == 3){
+          if (args.size() == 4){
             switch(args.get(0)) {
               case "addmachineexercise": addMachineExercise(args.get(1), args.get(2), args.get(3));
                               break;
@@ -73,7 +73,7 @@ public class UI extends Sql {
             }
           }
 
-          /*if (args.size() > 3){
+          /*if (args.size() > 4){
             switch(args.get(0)) {
             case "addexercise": addExercise(args.get(1), args.get(2), new ArrayList<String>(Arrays.asList(args.subList(3, args.size())));
                               break;
