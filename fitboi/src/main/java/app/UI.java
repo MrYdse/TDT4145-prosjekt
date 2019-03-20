9cdc71896c718f28368fcd8dac6d4f625db69fa3
axelk <axelkjnsberg@gmail.com>
2019-03-20 15:40:47 +0100

    Update UI
    Added interpretation of user input. Splits input by whitespace and uses 
    the first word to call a method and the rest as arguments. Also cheks if 
    the amount of arguments is correct and if you are calling a existing 
    method.

--- fitboi/src/main/java/app/UI.java
+++ fitboi/src/main/java/app/UI.java
@@ -3,6 +3,7 @@ package app;
 import java.sql.*;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,26 +12,76 @@ public class UI extends Sql {
     private PrintStream printStream;
 
     private Map<String, String> methods = new HashMap<String, String>();
-    private String format = "%-40s%s%n";
+    private String format = "%-50s%s%n";
 
     public UI(PrintStream stream) {
         this.printStream = stream;
         this.connect();
+
         methods.put("Help", "Prints list of methods");
+        methods.put("Add user", "Syntax: AddUser");
+        methods.put("Add exercise group", "Syntax: AddExerciseGroup <name>");
     }
 
     public void handleInput(String input) {
-        switch(input) {
-            case "hi":      printStream.print("Hi yourself");
-                            break;
-            case "test":    printStream.print("Yup, that's a test");
-                            break;
+        ArrayList<String> args = new ArrayList<String>(Arrays.asList(input.toLowerCase().split(" ")));
+          if (args.size() == 0){
+            switch(args.get(0)) {
             case "help":    for (Map.Entry<String, String> entry : methods.entrySet()) {
                                   System.out.printf(format, entry.getKey(), entry.getValue());
                             }
                             break;
+            case "adduser": addUser();
+                            break;
+            case "listexercisegroups": listExerciseGroups();
+                            break;
+            default: System.out.println("Is your method in the method list, and/or have you specified the required arguments?");
+                            break;
+            }
+          }
+
+          if (args.size() == 1){
+            switch(args.get(0)) {
+            case "addexercisegroup": addExerciseGroup(args.get(1));
+                            break;
+            case "addfreeexercise": addFreeExercise(args.get(1));
+                            break;
+            default: System.out.println("Is your method in the method list, and/or have you specified the required arguments?");
+                            break;
+            }
+          }
+
+          if (args.size() == 2){
+            switch(args.get(0)) {
+            case "addmachine": addMachine(args.get(1), args.get(2));
+                            break;
+            case "connectexercisetogroup": connectExerciseToGroup(args.get(1), args.get(2));
+                            break;
+            default: System.out.println("Is your method in the method list, and/or have you specified the required arguments?");
+                            break;
+            }
+          }
+
+          if (args.size() == 3){
+            switch(args.get(0)) {
+              case "addmachineexercise": addMachineExercise(args.get(1), args.get(2), args.get(3));
+                              break;
+              case "addexercise": addExercise(args.get(1), args.get(2), new ArrayList<String>(Arrays.asList(args.get(3))));
+                              break;
+              default: System.out.println("Is your method in the method list, and/or have you specified the required arguments?");
+                              break;
+            }
+          }
+
+          /*if (args.size() > 3){
+            switch(args.get(0)) {
+            case "addexercise": addExercise(args.get(1), args.get(2), new ArrayList<String>(Arrays.asList(args.subList(3, args.size())));
+                              break;
+            default: System.out.println("Is your method in the method list, and/or have you specified the required arguments?");
+                            break;
+            }
+          }*/
         }
-    }
 
     private void addUser() {
 
@@ -52,7 +103,7 @@ public class UI extends Sql {
 
     }
 
-    private void connectExerciseToGroup(String exerciseID) {
+    private void connectExerciseToGroup(String exerciseID, String exerciseGroupID) {
 
     }
 
